16380fdc9798e951957b5a9a9c3e04051ce00de1
rkashaev <kashaev.rr@gmail.com>
2017-02-24 00:16:36 +0300

    second version

--- ai-to-psd.js
+++ ai-to-psd.js
@@ -1,34 +1,49 @@
 var GRADIENT = "GradientColor";
 var PATTERN = "PatternColor";
 
-
-if (documents.length > 0 && activeDocument.pathItems.length > 0){
+if (documents.length > 0) {
  
-    var allPaths = [];
-    app.executeMenuCommand('unlockAll');
-    app.executeMenuCommand('deselectall');
-    app.executeMenuCommand('selectall');
+	app.executeMenuCommand('unlockAll');
+	app.executeMenuCommand('deselectall');
+
+	for (var i = 0; i < activeDocument.pathItems.length; i++) {
+		var cp = activeDocument.pathItems[i];
+		if (isForLiveOutline(cp)) {
+			cp.selected = true;
+		}
+	}
     app.executeMenuCommand('Live Outline Stroke');
     app.executeMenuCommand('expandStyle');
     app.executeMenuCommand('deselectall');
     
-    for (var i=0; i < activeDocument.pathItems.length; i++){
-        allPaths[i] = activeDocument.pathItems[i];
-    }
-    
-    for (var i=0; i < allPaths.length; i++){
-        var cp = allPaths[i];
-        cp.selected = false;
-        var fillType = cp.fillColor.typename;
-        if (cp.closed && cp.filled  && 
-          !(cp.stroked || fillType == PATTERN || fillType == GRADIENT)) {
-            try {
-                cp.selected = true;  
-                app.doScript('Psd', 'My');
-            } catch (err) {
-                alert(err);
-            }
-            cp.selected = false;
-        }
-    }
+    var allPaths = [];
+	var allPaths = activeDocument.pathItems;
+	
+	for (var i = 0; i < allPaths.length; ) {
+		var cp = allPaths[i];
+		try {
+			var fillType = cp.fillColor.typename;
+			if (cp.closed && cp.filled  && 
+			  !(cp.stroked || fillType == PATTERN || fillType == GRADIENT)) {
+				cp.selected = true;
+				app.doScript('Psd', 'My');
+				cp.selected = false;
+// since PathItem become CompoundShape, allPaths will be reduced
+			} else {
+                i++;
+			}
+		} catch (err) {
+			alert(err);
+             i++;
+		}
+	}
+}
+
+//без заливки + есть обводка +  обводка не градиент и не паттерн
+function isForLiveOutline(obj) {
+    try {
+        var strokeType = obj.strokeColor.typename;
+        return (!cp.filled && strokeType != GRADIENT && strokeType != PATTERN);
+    } catch(e) {}
+    return false;    
 }
